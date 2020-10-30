f934e60ba7971a8b476f3e0e881c73fe8ed1766c
ASUS <ASUS@ASUS-X541U>
2020-10-30 19:32:24 +0700

    Program ini bertujuan menampilkan kelipatan 3.5

---
+++ src/pboif2/pkg10119044/latihan18/kelipatan/PBOIF210119044Latihan18Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119044.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Yohana Sri Rejeki
+ * KELAS    : IF2
+ * NIM      : 10119044
+ * Deskripsi Program : Program ini bertujuan menampilkan kelipatan 3.5
+ */
+
+public class PBOIF210119044Latihan18Kelipatan {
+    public static void main(String[] args) {
+        double i = 3.5;
+        while (i <= 35.0){
+            System.out.println(i);
+            
+            i = i + 3.5;
+        }
+    }
+    
+}
